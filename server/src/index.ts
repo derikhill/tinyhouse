0ac9a9c88e9ab17af36fb06f40de7cfc4ce2ceb2
derikhill <derikhill05@gmail.com>
2020-04-11 13:43:13 -0500

    modfying resolver and db connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,23 @@
-import express from 'express';
+require("dotenv").config();
+
+import express, { Application } from 'express';
 import {ApolloServer} from 'apollo-server-express';
+import {connectDatabase} from './database';
 import {typeDefs, resolvers} from './graphql';
 
-const app = express();
-const port = 9000;
-const server = new ApolloServer({typeDefs, resolvers});
 
-server.applyMiddleware({app, path: '/api'})
-app.listen(port);
+const mount = async (app: Application) => {
+    const db = await connectDatabase();
+    const server = new ApolloServer({
+        typeDefs, 
+        resolvers,
+        context: () => ({db})
+    });
+    server.applyMiddleware({app, path: '/api'})
 
-console.log(`[app]: http://localhost:${port}`);
+    app.listen(process.env.PORT);
+
+    console.log(`[app]: http://localhost:${process.env.PORT}`);
+}
+
+mount(express());
